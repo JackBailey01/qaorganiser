fbb0594f6f0faaa90d763601d6e23eccfee5b66c
Framsida <Joshuastait@gmail.com>
2018-09-18 14:54:51 +0100

    promise

--- App.js
+++ App.js
@@ -2,60 +2,102 @@ let MongoClient = require('mongodb').MongoClient
 let express = require("express");
 let app = express();
 app.set('views engine', "ejs");
-let async = require('async');
 
 function getTable() {
     return new Promise(resolve =>{
     MongoClient.connect('mongodb://matt:password1@ds159812.mlab.com:59812/qaorganiser', function (err, client) {
         if (err) throw err
         let db = client.db('qaorganiser')
-        db.collection('schedule').find().toArray(function (err, result) {
+        db.collection('schedule').find().toArray( function (err, result) {
             if (err) throw err;
             let result2 = [];
-            result.forEach(function (doc) {
+            let i = result.length;
+            result.forEach(async function (doc) {
                 let doc1 = [];
-                db.collection('skills').findOne({Sid: doc.Sid}, function (err, resultSkill) {
-                    if (err) throw err;
-                    let skillName = resultSkill.Skill
-                    doc1.push(skillName)
-
-                })
-                db.collection('roomInfo').findOne({Rid: doc.Roomid}, function (err, resultRoom) {
-                    if (err) throw err;
-                    let RoomName = resultRoom.Title
-                    doc1.push(RoomName)
-
-                })
-                db.collection('trainerInfo').findOne({Tid: doc.Trainerid}, function (err, resultTrainer) {
-                    if (err) throw err;
-                    let TrainerName = resultTrainer.Name
-                    doc1.push("Trainerid", TrainerName)
-
-                })
-                db.collection('groupInfo').findOne({Gid: doc.Groupid}, function (err, resultGroup) {
-                    if (err) throw err;
-                    let GroupName = resultGroup.Name
-                    doc1.push(GroupName)
-                    result2.push(doc1);
-
-                })
+                let trainer = await getTrainer(doc);
+                let skill = await getSkill(doc);
+                let room = await getRoom(doc);
+                let groupInfo = await getGroupInfo(doc);
+                doc1.push(trainer);
+                doc1.push(skill);
+                doc1.push(room);
+                doc1.push(groupInfo);
+                doc1.push(doc.Date);
+                result2.push(doc1);
+                if (i == 1) {
+                    resolve(result2)
+                }
+                else {
+                    i--
+                }
 
             })
-            resolve(result2)
-
 
         })
-
 
     })
 })
 }
 
+function getTrainer(doc){
+    return new Promise(resolve => {
+        MongoClient.connect('mongodb://matt:password1@ds159812.mlab.com:59812/qaorganiser', function (err, client) {
+            if (err) throw err
+            let db = client.db('qaorganiser')
+            db.collection('trainerInfo').findOne({Tid: doc.Trainerid}, function (err, resultTrainer) {
+                if (err) throw err;
+                resolve(resultTrainer.Name)
+            })
+        })
+    })
+}
+
+function getSkill(doc){
+    return new Promise(resolve => {
+
+        MongoClient.connect('mongodb://matt:password1@ds159812.mlab.com:59812/qaorganiser', function (err, client) {
+            if (err) throw err
+            let db = client.db('qaorganiser')
+            db.collection('skills').findOne({Sid: doc.Sid}, function (err, resultSkill) {
+                if (err) throw err;
+                resolve(resultSkill.Skill)
+            })
+        })
+    })
+}
+function getRoom(doc){
+    return new Promise(resolve => {
+
+        MongoClient.connect('mongodb://matt:password1@ds159812.mlab.com:59812/qaorganiser', function (err, client) {
+            if (err) throw err
+            let db = client.db('qaorganiser')
+            db.collection('roomInfo').findOne({Rid: doc.Roomid}, function (err, resultRoom) {
+                if (err) throw err;
+                resolve(resultRoom.Title)
+            })
+        })
+    })
+}
+function getGroupInfo(doc){
+    return new Promise(resolve =>{
+    MongoClient.connect('mongodb://matt:password1@ds159812.mlab.com:59812/qaorganiser', function (err, client) {
+        if (err) throw err
+        let db = client.db('qaorganiser')
+        db.collection('groupInfo').findOne({Gid: doc.Groupid}, function (err, resultGroup) {
+            if (err) throw err;
+            resolve(resultGroup.Name);
+
+        })
+    })
+    })
+}
+
 app.get('/', async function(req, res) {
     let result2 = await getTable();
-    console.log(result2)
     res.render('Homepage', {result2});
 })
+
+
 
 
 
